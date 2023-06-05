d91a5823fa7357c841b6ca65c3bdcca4278ca011
Omar Walid <omarwalidhamed@gmail.com>
2023-06-05 22:22:05 +0300

    Implemented Upload Operation

---
+++ app/src/main/java/com/omar/pcconnector/operation/upload/UploadOperation.kt
@@ -0,0 +1,120 @@
+package com.omar.pcconnector.operation.upload
+
+import android.content.ContentResolver
+import androidx.documentfile.provider.DocumentFile
+import com.omar.pcconnector.network.api.FileSystemOperations
+import com.omar.pcconnector.network.api.getDataOrThrow
+import com.omar.pcconnector.operation.MonitoredOperation
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.merge
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.MediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import java.util.concurrent.CancellationException
+
+
+sealed class UploadOperationState(
+    val filesNames: List<String>
+) {
+    sealed class Initialized(
+        filesNames: List<String>,
+    ): UploadOperationState(filesNames) {
+
+        class Uploading(
+            filesNames: List<String>,
+            val currentlyUploadingFile: String,
+            val totalSize: Long,
+            val uploadedSize: Long
+        ): Initialized(filesNames)
+
+    }
+
+    class Initializing(
+        filesNames: List<String>
+    ): UploadOperationState(filesNames)
+    class Failed(filesNames: List<String>): UploadOperationState(filesNames)
+}
+
+
+private typealias UploadRequest = Pair<MultipartBody.Part, Flow<Pair<String, Float>>>
+private typealias UploadRequests = List<UploadRequest>
+
+class UploadOperation(
+    private val uploadApi: FileSystemOperations,
+    private val documents: List<DocumentFile>,
+    private val contentResolver: ContentResolver,
+    private val uploadPath: String
+): MonitoredOperation<UploadOperationState, Unit>() {
+
+    override val name: String
+        get() = "Upload Files"
+    override val operationDescription: String
+        get() = "Uploading ${documents.size} files"
+
+    override suspend fun start() {
+        if (documents.isEmpty()) throw IllegalArgumentException()
+        if (documents.size > 1 && documents.any {it.isDirectory}) throw Exception("You can upload either 1 directory or multiple files")
+        upload()
+    }
+
+    private suspend fun upload() = withContext(Dispatchers.IO){
+
+        val fileRequests = documents.flatMap { if (it.isFile) listOf(getFileRequestBody(it, listOf("."))) else getDirectoryRequestBodies(it) }
+        val parts = fileRequests.map { it.first }
+        val progresses = fileRequests.map { it.second }
+            .merge()
+
+        val collectionJob = launch {
+            progresses.collect {
+
+            }
+        }
+
+        val destinationPart = MultipartBody.Part.createFormData(
+            "dest", null,
+            RequestBody.create(MediaType.get("text/plain"), uploadPath)
+        )
+        uploadApi.upload(
+            parts.toMutableList().apply { add(destinationPart) }
+        ).execute().body().getDataOrThrow()
+
+        collectionJob.cancel()
+    }
+
+
+    private fun getFileRequestBody(file: DocumentFile, folders: List<String>): UploadRequest {
+        val progressFlow = MutableStateFlow(-1.0f)
+        val requestBody = UploadRequestBody(file.uri, contentResolver, progressFlow)
+        return  MultipartBody.Part.createFormData(folders.joinToString("/").ifEmpty { "." }, file.name ?: "Unknown", requestBody) to
+                progressFlow.map { progress -> (file.name ?: "Unknown") to progress }
+    }
+
+    private fun getDirectoryRequestBodies(directory: DocumentFile, folders: List<String> = listOf()): UploadRequests
+    {
+        val newFolders = mutableListOf<String>().apply { addAll(folders); add(directory.name ?: "Unknown Directory") }
+        val requestBodies = mutableListOf<UploadRequest>()
+        directory.listFiles()
+            .forEach {
+                if (it.isDirectory) requestBodies.addAll(getDirectoryRequestBodies(it, newFolders))
+                else requestBodies.add(getFileRequestBody(it, newFolders))
+            }
+        return requestBodies
+    }
+
+    override suspend fun cancel() {
+        TODO("Not yet implemented")
+    }
+
+    private val _progress = MutableStateFlow<UploadOperationState>(UploadOperationState.Initializing(listOf()))
+
+    override val progress: StateFlow<UploadOperationState>
+        get() = _progress
+}
---
+++ app/src/main/java/com/omar/pcconnector/operation/upload/UploadRequestBody.kt
@@ -0,0 +1,51 @@
+package com.omar.pcconnector.operation.upload
+
+import android.content.ContentResolver
+import android.net.Uri
+import kotlinx.coroutines.flow.MutableStateFlow
+import okhttp3.MediaType
+import okhttp3.RequestBody
+import okio.BufferedSink
+
+
+private const val BUFFER_SIZE = 4096
+
+class UploadRequestBody(
+    private val fileUri: Uri,
+    private val contentResolver: ContentResolver,
+    private val progressFlow: MutableStateFlow<Float>
+): RequestBody() {
+
+
+    override fun contentType(): MediaType? {
+        return null
+    }
+
+    override fun contentLength(): Long {
+        return contentResolver.openFileDescriptor(fileUri, "r").use { it?.statSize ?: -1L }
+    }
+
+    override fun writeTo(sink: BufferedSink) {
+        contentResolver.openInputStream(fileUri)
+            .use { fileStream ->
+
+                    if (fileStream == null) throw Exception("Failed to open the file")
+                    emitProgress(0.0f)
+                    val totalSize = fileStream.available()
+                    var totalSent = 0L
+                    val buffer = ByteArray(BUFFER_SIZE)
+                    while (totalSent < totalSize) {
+                        val read = fileStream.read(buffer, 0, BUFFER_SIZE)
+                        sink.write(buffer, 0, read)
+                        totalSent += read
+                        emitProgress(totalSent / totalSize.toFloat())
+                    }
+                    emitProgress(1.0f)
+
+            }
+    }
+
+    private fun emitProgress(percentage: Float) {
+        progressFlow.value = percentage
+    }
+}
